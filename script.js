43edc1e86d7971ca0abd2c4798dcf6a3e15cbcdf
Aditya Raj <154418353+Codeadityaa@users.noreply.github.com>
2026-08-15 13:25:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 function shortcut(s1, s2) {
-  // your code here
-	if (s1 === "" || s2 === "") {
-    return "";
-}
-}
-return s1[0] + s2[0];
-// Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
+    if (s1 === "" || s2 === "") {
+        return "";
+    }
+} // <--- Function yahan khatam ho gaya!
+return s1[0] + s2[0]; // <--- Yeh bechara bahar akela ro raha hai! (Error dega)
+
